8abb9950aabca9464007a9989b9ab27623bffc6f
Gauraang Goel <ggauraang@gmail.com>
2024-06-13 17:31:29 +0530

    ui added

--- Assets/UIManagerScene.cs
+++ Assets/UIManagerScene.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManagerScene : MonoBehaviour
 {
     public GameObject task1;
+    public GameObject task2;
+    public GameObject task21;
     public GameObject allow;
+    public GameObject buttonui;
+    public GameObject lever1ui;
+    public GameObject lever2ui;
     void Start()
     {
         
@@ -15,6 +21,17 @@ public class UIManagerScene : MonoBehaviour
     {
         task1.SetActive(false);
         allow.SetActive(true);
-
+    }
+    public void SecondTask()
+    {
+        task2.SetActive(false);
+        task21.SetActive(false);
+        allow.SetActive(true);
+    }
+    public void buttonpressed()
+    {
+        buttonui.SetActive(false);
+        lever1ui.SetActive(false);
+        lever2ui.SetActive(false);
     }
 }
