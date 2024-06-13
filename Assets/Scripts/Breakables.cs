79712947f548de9ccda9bc8d81c96b1ed4b08e9e
Kartik <kartik242421@gmail.com>
2024-06-13 18:24:11 +0530

    gun functionality completed

---
+++ Assets/Scripts/Breakables.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breakables : MonoBehaviour
+{
+
+    public List<GameObject> breakablePieces;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(var item in breakablePieces)
+        {
+            item.SetActive(false);
+        }
+    }
+
+    public void Break()
+    {
+        foreach(var item in breakablePieces)
+        {
+            item.SetActive(true);
+            item.transform.parent = null;
+        }
+        gameObject.SetActive(false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
--- Assets/Scripts/MeteorPistol.cs
+++ Assets/Scripts/MeteorPistol.cs
@@ -6,6 +6,12 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class MeteorPistol : MonoBehaviour
 {
     public ParticleSystem particles;
+
+    public LayerMask layerMask;
+    public Transform shootSource;
+    public float distance = 10;
+
+    private bool rayActivate = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,16 +23,31 @@ public class MeteorPistol : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (rayActivate)
+        {
+            RaycastCheck();
+        }
     }
 
     public void StartShoot()
     {
         particles.Play();
+        rayActivate = true;
     }
     public void StopShoot()
     {
         particles.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+        rayActivate = false;
 
+    }
+
+    void RaycastCheck()
+    {
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(shootSource.position, shootSource.forward, out hit, distance, layerMask);
+        if (hasHit)
+        {
+            hit.transform.gameObject.SendMessage("Break", SendMessageOptions.DontRequireReceiver);
+        }
     }
 }
