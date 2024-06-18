71a813a8a97a3e94cf49e8f67ccc7f6950a9a626
Kartik <kartik242421@gmail.com>
2024-06-18 17:50:21 +0530

    button function completed

---
+++ Assets/Scripts/ButtonPush.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+
+public class ButtonPush : MonoBehaviour
+{
+
+    public GameObject defenceSystemLight;
+    public GameObject warningSoundLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        defenceSystemLight.SetActive(false);
+
+        GetComponent<XRSimpleInteractable>().selectEntered.AddListener(x => activateDefenceSystem());
+    }
+
+    public void activateDefenceSystem()
+    {
+        warningSoundLight.SetActive(false);
+        defenceSystemLight.SetActive(true);
+    }
+}
