637074c71a54ea7b7a14e14062c9889574bd8178
Gauraang Goel <ggauraang@gmail.com>
2024-06-19 16:18:41 +0530

    timeline

--- Assets/Scripts/ButtonPush.cs
+++ Assets/Scripts/ButtonPush.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.Events;
+
 
 
 public class ButtonPush : MonoBehaviour
