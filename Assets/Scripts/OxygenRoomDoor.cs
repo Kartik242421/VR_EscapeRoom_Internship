5298d7a51a7ea88e2acc20da945fc6d5b6f499e9
Kartik <kartik242421@gmail.com>
2024-06-26 16:07:10 +0530

    lever functionality completed

--- Assets/Scripts/OxygenRoomDoor.cs
+++ Assets/Scripts/OxygenRoomDoor.cs
@@ -2,16 +2,16 @@ using UnityEngine;
 
 public class OxygenRoomDoor : MonoBehaviour
 {
-    public Vector3 localTargetPosition; // Target position to move the object to
-    public float speed = 2.0f; // Speed at which the object moves
+    public Vector3 localTargetPosition; 
+    public float speed = 2.0f; 
 
-    private Vector3 initialPosition; // Initial position of the object
-    public bool isMovingToTarget = false; // Flag to check if the object is moving towards the target
+    
+    public bool isMovingToTarget = false;
+
     //private AudioSource audioSource; // Audio source component
 
     void Start()
     {
-        initialPosition = transform.position; // Store the initial position
         //audioSource = GetComponent<AudioSource>(); // Get the audio source component
     }
 
@@ -19,9 +19,10 @@ public class OxygenRoomDoor : MonoBehaviour
     {
         if (isMovingToTarget)
         {
-            Debug.Log("Moving Object");
+            Debug.Log("Moving Object....");
             // Move towards the target position
             transform.localPosition = Vector3.Lerp(transform.localPosition, localTargetPosition, Time.deltaTime * speed);
         }
+        
     }
 }
